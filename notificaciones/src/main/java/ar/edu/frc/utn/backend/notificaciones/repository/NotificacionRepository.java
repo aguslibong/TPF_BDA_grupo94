aa8398846fec726900eefe1782cf70930af4a454
LordLucky04 <94505@sistemas.frc.utn.edu.ar>
2024-10-29 17:06:33 -0300

    Terminado logica de posiciones

--- notificaciones/src/main/java/ar/edu/frc/utn/backend/notificaciones/repository/NotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.frc.utn.backend.notificaciones.repository;
-
-import ar.edu.frc.utn.backend.notificaciones.entities.Notificacion;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificacionRepository extends CrudRepository<Notificacion, Integer> {
-}
