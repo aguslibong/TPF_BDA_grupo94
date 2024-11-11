9549ec9cebd797a8878208c3756d806d21abfc11
LordLucky04 <94505@sistemas.frc.utn.edu.ar>
2024-11-11 18:39:04 -0300

    Últimas modificaciones

---
+++ gateway/src/test/java/ar/edu/utn/frc/backend/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.backend.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
